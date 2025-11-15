ab636ec0c133c9c69bf8c36213dc95ddc861478a
Gabriel Petry <24570030+gabrielpetry@users.noreply.github.com>
2025-11-15 07:43:35 -0300

    fix

--- scripts/changed-containers.sh
+++ scripts/changed-containers.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 event_before="${1:-}"
 sha="${2:-}"
 
-changed="$(git diff --name-only "${event_before}" "${sha}" | grep -Ev '.github|.scripts' | cut -d'/' -f1 | sort -u || echo '')"
+changed="$(git diff --name-only "${event_before}" "${sha}" | grep -Ev '.github|.scripts' | cut -d'/' -f1 | sort -u | tr '\n' ' ' | xargs || echo '')"
 
 echo "containers=${changed}"
