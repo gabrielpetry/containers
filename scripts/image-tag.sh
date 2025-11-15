b7a507c982ebf0c7047d7214ad4dc8f87a57e170
Gabriel Petry <24570030+gabrielpetry@users.noreply.github.com>
2025-11-15 08:04:40 -0300

    it has to be unquoted

--- scripts/image-tag.sh
+++ scripts/image-tag.sh
@@ -8,7 +8,7 @@ dir="$1"
 	exit 1
 }
 
-version="$(grep -i 'label version' "${dir}/Dockerfile" | cut -d' ' -f2 || true)"
+version="$(grep -i 'label version' "${dir}/Dockerfile" | cut -d' ' -f2 | sed 's/"//g' || true)"
 
 [[ -z "${version}" ]] && {
 	echo "No version label found in ${dir}/Dockerfile must be set with 'LABEL version=\"x.y.z\"'"
