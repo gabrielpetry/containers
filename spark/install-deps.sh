8e2a770ea7474b7759cf918b2147866d56bee22d
gabriel petry <24570030+gabrielpetry@users.noreply.github.com>
2026-01-12 12:51:33 -0300

    fix

--- spark/install-deps.sh
+++ spark/install-deps.sh
@@ -5,4 +5,10 @@ set -euo pipefail
 for url in "$@"; do
 	filename="$(basename "${url}")"
 	curl -L -o "/opt/spark/jars/${filename}" "${url}"
+	if [[ -f "/opt/spark/jars/${filename}" ]]; then
+		echo "Downloaded ${filename} to /opt/spark/jars/"
+	else
+		echo "Failed to download ${filename}"
+		exit 1
+	fi
 done
